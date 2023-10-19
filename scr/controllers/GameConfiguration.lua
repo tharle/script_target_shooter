87e03f498a62c742c2c6111750e39dbcb04f35eb
tharle <tharles.josefidecamargo@gmail.com>
2023-10-19 03:19:23 -0400

    finish the game

--- scr/controllers/GameConfiguration.lua
+++ scr/controllers/GameConfiguration.lua
@@ -21,13 +21,13 @@ function GameConfiguration.new()
     o.resolution_hud = Vector.new(CONFIG_RESOLUTION_WIDTH, 50)
     o.limit_horizontal = Vector.new(0, CONFIG_RESOLUTION_WIDTH) -- limit horizontal
     o.limit_vertical = Vector.new(o.resolution_hud.y,CONFIG_RESOLUTION_HEIGHT - 40) -- limit vertical
-    o.pigeon_count_type_pigeons = 1
+    o.pigeon_count_type_pigeons = 6
     o.pigeons_amount_by_level = 5
     o.pigeon_width = 44
     o.pigeon_height = 36
     o.pigeon_velocity_animation = 0.5
-    o.pigeon_bounds_horizontal = Vector.new(400, 400)
-    o.pigeon_bounds_vertical = Vector.new(300, 300)
+    o.pigeon_bounds_horizontal = Vector.new(400, 410)
+    o.pigeon_bounds_vertical = Vector.new(450, 460)
     o.pigeon_limit_velocity = Vector.new(40, 70)
     o.pigeon_src_sprite = "assets/sprites/objects/pigeons/pigeon_fly_"
     o.pigeon_value = 50
